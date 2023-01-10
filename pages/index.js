3f6417dd508a161b73525a7a88c31ee6cb078647
jayjay1878 <jaybartley777@outlook.com>
2023-01-10 14:29:29 +0000

    homepage
    start writing the home page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 //* Description: This is the home page of the site
 
 // Import statements
-import React from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
-import '../styles/Home.module.css';
+import React from "react";
+import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import "../styles/Home.module.css";
 
 // Component imports
-import Navbar from '../components/Navbar/Navbar';
+import Navbar from "../components/Navbar/Navbar";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 // Home component
 export default function Home() {
@@ -24,9 +25,23 @@ export default function Home() {
       </Head>
       <main>
         <Navbar />
-        <h1 className="text-3xl font-bold underline flex justify-center items-center h-screen">
-          Site being built
-        </h1>
+        <div className="flex-col items-center justify-center w-2/3 p-6">
+          <h1 className="text-3xl font-bold">Learn (almost) anything!</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <div className="flex gap-3 ">
+            <Link className="py-1 px-4 bg-red-400 rounded-lg" href="/explore">
+              EXPLORE
+            </Link>
+            <Link className="py-1 px-4 bg-red-400 rounded-lg" href="/about">
+              ABOUT
+            </Link>
+          </div>
+        </div>
       </main>
     </>
   );
