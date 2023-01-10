4ada3947bf9ae22db6d3687c89cd3fadf10b8f8c
OttCoder <101673336+Ottcoder@users.noreply.github.com>
2023-01-10 16:05:25 +0000

    improved styling on Navbar and removed unused code

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -7,22 +7,11 @@ import Link from "next/link";
 // Navbar component
 export default function Navbar() {
   return (
-    <div>
-      <nav className="flex items-center justify-between flex-wrap bg-gray-400 p-6">
+    // add fixed to div
+    <div className="w-full p-6">
+      <nav className="flex items-center justify-between flex-wrap bg-gray-400 p-6 rounded-full">
         <div className="flex items-center flex-shrink-0 text-white mr-6">
           <span className="font-semibold text-xl tracking-tight">Logo</span>
-        </div>
-        <div className="block lg:hidden">
-          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-            <svg
-              className="fill-current h-3 w-3"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
         </div>
         <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
           <div className="text-sm lg:flex-grow">
@@ -45,6 +34,7 @@ export default function Navbar() {
               Explore
             </Link>
           </div>
+          <h3 className="pr-4 underline decoration-orange-400">Location</h3>
           <div>
             <a
               href="#"
