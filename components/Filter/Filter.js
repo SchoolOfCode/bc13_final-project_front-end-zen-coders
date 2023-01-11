c6bc8b0e0ec3dc6c4d28f05ce9545517198b9158
itsevgenii <smazanovich@icloud.com>
2023-01-11 17:20:08 +0000

    Explore page work in progress. End of the day.

---
+++ components/Filter/Filter.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+const Filter = () => {
+    return (
+        <div>
+        <ul className='flex flex-row justify-between bg-gray-300 py-4 rounded-full my-9 mx-5 px-10'>
+            <li className='w-28 h-28 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
+            <li className='w-28 h-28 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
+            <li className='w-28 h-28 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
+            <li className='w-28 h-28 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
+            <li className='w-28 h-28 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
+            <li className='w-28 h-28 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
+            <li className='w-28 h-28 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
+            <li className='w-28 h-28 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
+            <li className='w-28 h-28 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
+            <li className='w-28 h-28 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
+       
+            <li className='w-28 h-20 flex items-center justify-center text-5xl'><div>Filter</div></li>
+      
+          
+
+        </ul>
+        </div>
+    )
+}
+
+export default Filter
--- pages/explore/index.js
+++ pages/explore/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import ExploreList from '../../components/ExploreList/ExploreList';
 import SearchBar from '../../components/SearchBar/SearchBar';
+import Filter from '../../components/Filter/Filter';
 
 export default function index() {
   return (
     <div>
       <SearchBar />
+      <Filter/>
       <h1>Explore Page</h1>
       <ExploreList />
     </div>
