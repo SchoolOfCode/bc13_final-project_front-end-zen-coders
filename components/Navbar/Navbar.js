16b3295f11b7f7e34f19d3d9ece3cf537a468900
kianeblackman <kianeblackman@gmail.com>
2023-01-11 10:23:55 +0000

    svg logo added, image added and button styling added to about page

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -8,28 +8,26 @@ import Link from 'next/link';
 export default function Navbar() {
   return (
     // add fixed to div
-    <div className="w-full p-6">
-      <nav className="flex items-center justify-between flex-wrap bg-gray-300 p-6 rounded-full">
-        <div className="flex items-center flex-shrink-0 text-black mr-6">
-          <span className="font-semibold text-xl tracking-tight">Logo</span>
-        </div>
-        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div className="w-full bg-white p-6">
+      <nav className="flex flex-wrap items-center justify-between rounded-full border-2 border-black bg-white p-2">
+        <img src="/logo.svg" alt="logo" className="h-12" />
+        <div className="block w-full flex-grow lg:flex lg:w-auto lg:items-center">
           <div className="text-sm lg:flex-grow">
             <Link
               href="/"
-              className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-white mr-4"
+              className="mt-4 mr-4 block text-black hover:text-white lg:mt-0 lg:inline-block"
             >
               Home
             </Link>
             <Link
               href="/explore"
-              className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-white mr-4"
+              className="mt-4 mr-4 block text-black hover:text-white lg:mt-0 lg:inline-block"
             >
               Explore
             </Link>
             <Link
               href="/about"
-              className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-white"
+              className="mt-4 block text-black hover:text-white lg:mt-0 lg:inline-block"
             >
               About
             </Link>
@@ -38,9 +36,9 @@ export default function Navbar() {
           <div>
             <a
               href="#"
-              className="inline-block text-sm px-4 py-2 leading-none border rounded-full text-black border-black hover:border-transparent hover:text-white hover:bg-black mt-4 lg:mt-0"
+              className="mt-4 inline-block  px-4 py-2 text-lg leading-none text-indigo-700 hover:border-transparent  hover:text-white lg:mt-0"
             >
-              P
+              PK
             </a>
           </div>
         </div>
--- components/SearchBar/SearchBar.js
+++ components/SearchBar/SearchBar.js
@@ -6,18 +6,12 @@ import React from 'react';
 // SearchBar component
 export default function SearchBar() {
   return (
-    <div class="flex justify-center w-full">
-      <div class="mb-3">
-        <div class="input-group relative flex flex-wrap mb-4 rounded">
-          <input
-            type="search"
-            class="rounded-full  form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300  transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-            placeholder="Search"
-            aria-label="Search"
-            aria-describedby="button-addon2"
-          ></input>
-        </div>
-      </div>
+    <div className='px-6'>
+      <input
+        type="search"
+        class="form-control  relative m-0 block w-full min-w-0 flex-auto rounded-full border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-1.5 text-base font-normal  text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
+        placeholder="Search"
+      ></input>
     </div>
   );
 }
