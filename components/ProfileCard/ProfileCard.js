1c265ea1d6b367bb4d1c00fba0f52fdec317f16c
Ilwad Kamal <112329600+ilwadk@users.noreply.github.com>
2023-01-11 16:18:38 +0000

    small errors changed

--- components/ProfileCard/ProfileCard.js
+++ components/ProfileCard/ProfileCard.js
@@ -1,42 +1,39 @@
-import React from 'react';
+import React from "react";
 
 export default function ProfileCard() {
   return (
-    <div class="h-screen w-1/4 rounded-lg border-gray-700 bg-gray-800 shadow-md">
+    <div className="h-screen w-1/4 rounded-lg border-gray-700 bg-gray-800 shadow-md">
       <a>
         <img
-          class="rounded-t-lg"
+          className="rounded-t-lg"
           src="/profile-image.jpg"
           alt="profile picture"
         />
       </a>
-      <div class="p-5">
+      <div className="p-5">
         <div>
-          <h1>
-            <h1 class="mb-2 text-4xl font-bold tracking-tight text-white">
-              Sharer Name
-            </h1>
+          <h1 className="mb-2 text-4xl font-bold tracking-tight text-white">
+            Sharer Name
           </h1>
           <h3>Locaiton</h3>
           <div>
             <p>star</p>
           </div>
           <h2>About me:</h2>
-          <p class="mb-3 font-serif text-white">
+          <p className="mb-3 font-serif text-white">
             Here are the biggest enterprise technology acquisitions of 2021 so
             far, in reverse chronological order.
           </p>
-          <div></div>
         </div>
         <div>
           <div>
             <p>skill</p>
           </div>
-          <a class="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <a className="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Contact
             <svg
               aria-hidden="true"
-              class="ml-2 -mr-1 h-4 w-4"
+              className="ml-2 -mr-1 h-4 w-4"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
