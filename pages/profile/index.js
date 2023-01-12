eaea8f689073566ea2b8e058a46984da82af87d6
Ilwad Kamal <112329600+ilwadk@users.noreply.github.com>
2023-01-12 09:55:30 +0000

    profile event card added

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ProfileCard from '../../components/ProfileCard/ProfileCard';
-
+import React from "react";
+import ProfileCard from "../../components/ProfileCard/ProfileCard";
+import ProfileEventCard from "../../components/ProfileEventCard/profileEventCard";
 
 export default function index() {
   return (
-  <div>
-  <ProfileCard/>
-  </div>
-  )
+    <div>
+      <ProfileCard />
+      <ProfileEventCard />
+    </div>
+  );
 }
