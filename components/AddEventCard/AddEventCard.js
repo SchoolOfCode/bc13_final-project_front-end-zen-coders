8d94db80b1dd815b9ffbb15df5170ed352d04829
kianeblackman <kianeblackman@gmail.com>
2023-01-12 17:39:24 +0000

    input types added

--- components/AddEventCard/AddEventCard.js
+++ components/AddEventCard/AddEventCard.js
@@ -2,22 +2,35 @@ import React from 'react';
 
 export default function AddEventCard() {
   return (
-    <card className="mb-6 flex flex-col rounded-lg border-2 border-black bg-gray-100 p-3">
+    <card className="mb-6 flex flex-col rounded-lg border-2 border-black bg-gray-200 p-3">
       <div className="flex flex-row">
-        <h2 className= "text-2xl font-bold">Skill Type: </h2>
+        <h2 className="text-2xl font-bold">Skill Type: </h2>
         <div className="flex flex-row">
           <h2>skill</h2>
           <h2>skill</h2>
           <h2>skill</h2>
         </div>
       </div>
-      <div className="flex flex-row gap-3 mt-3">
-        <input placeholder="Date" className="form-control  relative m-0 block w-full min-w-0 flex-auto rounded-full border border-solid border-gray-300 bg-gray-200 bg-clip-padding px-3 py-1.5 text-base font-normal  text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none" />
-        <input placeholder="Time" className="form-control  relative m-0 block w-full min-w-0 flex-auto rounded-full border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-1.5 text-base font-normal  text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none" />
+      <div className="mt-3 flex flex-row gap-3">
+        <input
+          placeholder="Date:"
+          type="date"
+          className="form-control  relative m-0 block w-full min-w-0 flex-auto rounded-full border border-solid border-gray-300 bg-gray-100 bg-clip-padding px-3 py-1.5 text-base font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
+        />
+        <input
+          placeholder="Time:"
+          type="time"
+          className="form-control  relative m-0 block w-full min-w-0 flex-auto rounded-full border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-1.5 text-base font-normal  text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
+        />
       </div>
-      <div className="flex flex-row justify-between gap-3  mt-3">
-        <input placeholder="Description" className="form-control  relative m-0 block w-full min-w-0 flex-auto rounded-full border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-1.5 text-base font-normal  text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none" />
-        <button className="rounded-full border-2 border-indigo-700 py-1 px-4 transition ease-in-out hover:scale-110 hover:bg-indigo-400 hover:bg-opacity-50">
+      <div className="mt-3 flex flex-row justify-between  gap-3">
+        <textarea
+          placeholder="Description:"
+          type="text"
+          rows="5"
+          className="form-control  relative m-0 block w-full min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-1.5 text-base font-normal  text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
+        />
+        <button className="h-full rounded-full border-2 border-indigo-700 object-contain py-1 px-4 transition ease-in-out hover:scale-110 hover:bg-indigo-400 hover:bg-opacity-50">
           Post
         </button>
       </div>
