c958d5ebd204219b67b2173efa38bd07e270911d
kianeblackman <kianeblackman@gmail.com>
2023-01-12 09:44:02 +0000

    error

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -10,35 +10,33 @@ export default function Navbar() {
     // add fixed to div
     <div className="w-full bg-white p-6">
       <nav className="flex flex-wrap items-center justify-between rounded-full border-2 border-black bg-white p-2">
-        <img src="/logo.svg" alt="logo" className="h-12 pr-3" />
+        <img src="/logo.svg" alt="logo" className="h-12" />
         <div className="block w-full flex-grow lg:flex lg:w-auto lg:items-center">
-          <div className="text-m lg:flex-grow">
+          <div className="text-sm lg:flex-grow">
             <Link
               href="/"
-              className="mt-4 mr-4 block text-black hover:text-indigo-700 hover:underline lg:mt-0 lg:inline-block"
+              className="mt-4 mr-4 block text-black hover:text-white lg:mt-0 lg:inline-block"
             >
               Home
             </Link>
             <Link
               href="/explore"
-              className="mt-4 mr-4 block text-black hover:text-indigo-700 hover:underline lg:mt-0 lg:inline-block"
+              className="mt-4 mr-4 block text-black hover:text-white lg:mt-0 lg:inline-block"
             >
               Explore
             </Link>
             <Link
               href="/about"
-              className="mt-4 block text-black hover:text-indigo-700 hover:underline lg:mt-0 lg:inline-block"
+              className="mt-4 block text-black hover:text-white lg:mt-0 lg:inline-block"
             >
               About
             </Link>
           </div>
-          <h3 className="text-m pr-4 underline decoration-indigo-400">
-            Location
-          </h3>
+          <h3 className="pr-4 underline decoration-orange-400">Location</h3>
           <div>
             <Link
               href="/profile"
-              className="mt-4 inline-block px-4 py-2 text-2xl font-bold leading-none text-indigo-600 hover:border-transparent hover:text-black lg:mt-0"
+              className="mt-4 inline-block  px-4 py-2 text-lg leading-none text-indigo-700 hover:border-transparent  hover:text-white lg:mt-0"
             >
               PK
             </Link>
