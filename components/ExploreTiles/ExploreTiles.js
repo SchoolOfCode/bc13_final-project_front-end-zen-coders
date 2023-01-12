3ca0b9bbc0b95d6ca791b49f63ebf58b2e8a16b7
itsevgenii <smazanovich@icloud.com>
2023-01-12 14:18:27 +0000

    Completed first version of the mockup for expole page

--- components/ExploreTiles/ExploreTiles.js
+++ components/ExploreTiles/ExploreTiles.js
@@ -1,23 +1,205 @@
 import React from "react";
 
+
 export default function ExploreTiles() {
   return (
-    <div className="flex w-1/3 flex-col items-center justify-center bg-gray-400">
-      <div className="flex h-36 w-36 bg-gray-500 m-4 rounded-full">
-        <img
-          className="h-20 w-20 flex rounded-full object-contain m-4"
-          src="/arrow.svg"
-        />
-      </div>
-      <div className="px-5 pb-5">
-        <h1 className="font-bold">Skill</h1>
-        <p>Name Surname</p>
-        <div className="mt-2.5 mb-5 flex items-center">
-          <img className="h-30 w-24" src="/knitting.svg" />
-          <p>11:30</p>
-          <p>Thur 12 Dec</p>
-        </div>
-      </div>
+    <div className="flex flex-row flex-wrap">
+    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-36 p-7 m-auto ">
+    <div className="flex flex-col items-center justify-center bg-gray-400 m-3 rounded-3xl hover:bg-gray-800">
+<div className="self-end h-36 w-36 bg-gray-500 m-5 rounded-full flex justify-center items-center">
+  <img
+    className="h-20 w-20 object-contain p-2"
+    src="/knitting.svg"
+  />
+</div>
+<div className="px-5 pb-5">
+  <h1 className="font-bold  ">Skill</h1>
+  <p className=" ">Name Surname</p>
+  <div className="mt-2.5 mb-5 flex items-center">
+    <img className="h-30 w-24" src="/arrow.svg" />
+    <div className="flex flex-col -mb-12">
+    <p>11:30</p>
+    <p>Thur 12 Dec</p>
     </div>
+  </div>
+</div>
+</div>
+ <div className="flex flex-col items-center justify-center bg-gray-400 m-3 rounded-3xl hover:bg-gray-800">
+<div className="self-end h-36 w-36 bg-gray-500 m-5 rounded-full flex justify-center items-center">
+  <img
+    className="h-20 w-20 object-contain p-2"
+    src="/shears.svg"
+  />
+</div>
+<div className="px-5 pb-5">
+  <h1 className="font-bold  ">Skill</h1>
+  <p className=" ">Name Surname</p>
+  <div className="mt-2.5 mb-5 flex items-center">
+    <img className="h-30 w-24" src="/arrow.svg" />
+    <div className="flex flex-col -mb-12">
+    <p>11:30</p>
+    <p>Thur 12 Dec</p>
+    </div>
+  </div>
+</div>
+</div>
+<div className="flex flex-col items-center justify-center bg-gray-400 m-3 rounded-3xl hover:bg-gray-800">
+<div className="self-end h-36 w-36 bg-gray-500 m-5 rounded-full flex justify-center items-center">
+  <img
+    className="h-20 w-20 object-contain p-2"
+    src="/piano.svg"
+  />
+</div>
+<div className="px-5 pb-5">
+  <h1 className="font-bold  ">Skill</h1>
+  <p className=" ">Name Surname</p>
+  <div className="mt-2.5 mb-5 flex items-center">
+    <img className="h-30 w-24" src="/arrow.svg" />
+    <div className="flex flex-col -mb-12">
+    <p>11:30</p>
+    <p>Thur 12 Dec</p>
+    </div>
+  </div>
+</div>
+</div>
+<div className="flex flex-col items-center justify-center bg-gray-400 m-3 rounded-3xl hover:bg-gray-800">
+<div className="self-end h-36 w-36 bg-gray-500 m-5 rounded-full flex justify-center items-center">
+  <img
+    className="h-20 w-20 object-contain p-2"
+    src="/knitting.svg"
+  />
+</div>
+<div className="px-5 pb-5">
+  <h1 className="font-bold  ">Skill</h1>
+  <p className=" ">Name Surname</p>
+  <div className="mt-2.5 mb-5 flex items-center">
+    <img className="h-30 w-24" src="/arrow.svg" />
+    <div className="flex flex-col -mb-12">
+    <p>11:30</p>
+    <p>Thur 12 Dec</p>
+    </div>
+  </div>
+</div>
+</div>
+<div className="flex flex-col items-center justify-center bg-gray-400 m-3 rounded-3xl hover:bg-gray-800">
+<div className="self-end h-36 w-36 bg-gray-500 m-5 rounded-full flex justify-center items-center">
+  <img
+    className="h-20 w-20 object-contain p-2"
+    src="/knitting.svg"
+  />
+</div>
+<div className="px-5 pb-5">
+  <h1 className="font-bold  ">Skill</h1>
+  <p className=" ">Name Surname</p>
+  <div className="mt-2.5 mb-5 flex items-center">
+    <img className="h-30 w-24" src="/arrow.svg" />
+    <div className="flex flex-col -mb-12">
+    <p>11:30</p>
+    <p>Thur 12 Dec</p>
+    </div>
+  </div>
+</div>
+</div>
+<div className="flex flex-col items-center justify-center bg-gray-400 m-3 rounded-3xl hover:bg-gray-800">
+<div className="self-end h-36 w-36 bg-gray-500 m-5 rounded-full flex justify-center items-center">
+  <img
+    className="h-20 w-20 object-contain p-2"
+    src="/knitting.svg"
+  />
+</div>
+<div className="px-5 pb-5">
+  <h1 className="font-bold  ">Skill</h1>
+  <p className=" ">Name Surname</p>
+  <div className="mt-2.5 mb-5 flex items-center">
+    <img className="h-30 w-24" src="/arrow.svg" />
+    <div className="flex flex-col -mb-12">
+    <p>11:30</p>
+    <p>Thur 12 Dec</p>
+    </div>
+  </div>
+</div>
+</div>
+<div className="flex flex-col items-center justify-center bg-gray-400 m-3 rounded-3xl hover:bg-gray-800">
+<div className="self-end h-36 w-36 bg-gray-500 m-5 rounded-full flex justify-center items-center">
+  <img
+    className="h-20 w-20 object-contain p-2"
+    src="/knitting.svg"
+  />
+</div>
+<div className="px-5 pb-5">
+  <h1 className="font-bold  ">Skill</h1>
+  <p className=" ">Name Surname</p>
+  <div className="mt-2.5 mb-5 flex items-center">
+    <img className="h-30 w-24" src="/arrow.svg" />
+    <div className="flex flex-col -mb-12">
+    <p>11:30</p>
+    <p>Thur 12 Dec</p>
+    </div>
+  </div>
+</div>
+</div>
+<div className="flex flex-col items-center justify-center bg-gray-400 m-3 rounded-3xl hover:bg-gray-800">
+<div className="self-end h-36 w-36 bg-gray-500 m-5 rounded-full flex justify-center items-center">
+  <img
+    className="h-20 w-20 object-contain p-2"
+    src="/knitting.svg"
+  />
+</div>
+<div className="px-5 pb-5">
+  <h1 className="font-bold  ">Skill</h1>
+  <p className=" ">Name Surname</p>
+  <div className="mt-2.5 mb-5 flex items-center">
+    <img className="h-30 w-24" src="/arrow.svg" />
+    <div className="flex flex-col -mb-12">
+    <p>11:30</p>
+    <p>Thur 12 Dec</p>
+    </div>
+  </div>
+</div>
+</div>
+<div className="flex flex-col items-center justify-center bg-gray-400 m-3 rounded-3xl hover:bg-gray-800">
+<div className="self-end h-36 w-36 bg-gray-500 m-5 rounded-full flex justify-center items-center">
+  <img
+    className="h-20 w-20 object-contain p-2"
+    src="/knitting.svg"
+  />
+</div>
+<div className="px-5 pb-5">
+  <h1 className="font-bold  ">Skill</h1>
+  <p className=" ">Name Surname</p>
+  <div className="mt-2.5 mb-5 flex items-center">
+    <img className="h-30 w-24" src="/arrow.svg" />
+    <div className="flex flex-col -mb-12">
+    <p>11:30</p>
+    <p>Thur 12 Dec</p>
+    </div>
+  </div>
+</div>
+</div>
+<div className="flex flex-col items-center justify-center bg-gray-400 m-3 rounded-3xl hover:bg-gray-800">
+<div className="self-end h-36 w-36 bg-gray-500 m-5 rounded-full flex justify-center items-center">
+  <img
+    className="h-20 w-20 object-contain p-2"
+    src="/knitting.svg"
+  />
+</div>
+<div className="px-5 pb-5">
+  <h1 className="font-bold  ">Skill</h1>
+  <p className=" ">Name Surname</p>
+  <div className="mt-2.5 mb-5 flex items-center">
+    <img className="h-30 w-24" src="/arrow.svg" />
+    <div className="flex flex-col -mb-12">
+    <p>11:30</p>
+    <p>Thur 12 Dec</p>
+    </div>
+  </div>
+</div>
+</div>
+        </div>
+</div>
   );
 }
+
+
+
+
--- components/Filter/Filter.js
+++ components/Filter/Filter.js
@@ -4,17 +4,18 @@ import React from 'react'
 const Filter = () => {
     return (
         <div>
-        <ul className='flex flex-row justify-between bg-gray-300 py-4 rounded-full my-9 mx-5 px-10'>
-            <li className='w-28 h-28 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
-            <li className='w-28 h-28 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
-            <li className='w-28 h-28 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
-            <li className='w-28 h-28 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
-            <li className='w-28 h-28 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
-            <li className='w-28 h-28 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
-            <li className='w-28 h-28 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
-            <li className='w-28 h-28 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
-            <li className='w-28 h-28 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
-            <li className='w-28 h-28 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
+        <ul className='flex flex-row justify-between bg-gray-300 py-2 rounded-full my-9 mx-5 px-6'>
+            <li className='w-20 h-20 flex items-center justify-center  bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
+            <li className='w-20 h-20 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
+            <li className='w-20 h-20 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
+            <li className='w-20 h-20 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
+            <li className='w-20 h-20 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
+            <li className='w-20 h-20 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
+            <li className='w-20 h-20 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
+            <li className='w-20 h-20 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
+            <li className='w-20 h-20 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
+            <li className='w-20 h-20 flex items-center justify-center bg-gray-900 rounded-full'><div><img src="/musicIcon.svg" alt="musicIcon" className='w-12'/></div></li>
+
        
             <li className='w-28 h-20 flex items-center justify-center text-5xl'><div>Filter</div></li>
       
