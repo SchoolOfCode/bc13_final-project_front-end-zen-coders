d314ba8cdf1a1734f7a9817ec134ade31fa4f5fd
kianeblackman <kianeblackman@gmail.com>
2023-01-12 14:57:16 +0000

    styling added to add new event card on profile page

--- components/AddEventCard/AddEventCard.js
+++ components/AddEventCard/AddEventCard.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default function AddEventCard() {
   return (
-    <card className=" flex flex-col border-2 border-black rounded-lg bg-red-300">
+    <card className="mb-6 flex flex-col rounded-lg border-2 border-black p-3">
       <div className="flex flex-row">
         <h2> Skill Type :</h2>
         <div className="flex flex-row">
-          <h2> skill</h2>
-          <h2> skill</h2>
-          <h2> skill</h2>
+          <h2>skill</h2>
+          <h2>skill</h2>
+          <h2>skill</h2>
         </div>
       </div>
       <div className="flex flex-row">
@@ -17,7 +17,10 @@ export default function AddEventCard() {
       </div>
       <div className="flex flex-row">
         <input placeholder="Description" />
-        <button className="rounded-full border-2 border-indigo-700 py-1 px-4 transition ease-in-out hover:scale-110 hover:bg-indigo-400 hover:bg-opacity-50"> Post </button>
+        <button className="rounded-full border-2 border-indigo-700 py-1 px-4 transition ease-in-out hover:scale-110 hover:bg-indigo-400 hover:bg-opacity-50">
+          {' '}
+          Post{' '}
+        </button>
       </div>
     </card>
   );
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { useState } from "react";
-import ProfileCard from "../../components/ProfileCard/ProfileCard";
-import ProfileEventCard from "../../components/ProfileEventCard/profileEventCard";
-import AddEventCard from "../../components/AddEventCard/AddEventCard.js";
+import React from 'react';
+import { useState } from 'react';
+import ProfileCard from '../../components/ProfileCard/ProfileCard';
+import ProfileEventCard from '../../components/ProfileEventCard/profileEventCard';
+import AddEventCard from '../../components/AddEventCard/AddEventCard.js';
 
 export default function index() {
   const [show, setShow] = useState(false);
@@ -11,9 +11,15 @@ export default function index() {
     <div className="grid grid-cols-4 gap-4 px-12">
       <ProfileCard className="col-span-1 bg-blue-200" />
       <div className="col-span-3">
-        <div className="mb-3 flex">
+        <div className="mb-3 flex justify-between">
           <h1 className="text-4xl font-bold">Available Sessions:</h1>
-          <button onClick={() => setShow(!show)} className="rounded-full border-2 border-indigo-700 py-1 px-4 transition ease-in-out hover:scale-110 hover:bg-indigo-400 hover:bg-opacity-50">+event</button>
+
+          <button
+            onClick={() => setShow(!show)}
+            className="rounded-full border-2 border-indigo-700 py-1 px-4 transition ease-in-out hover:scale-105 hover:bg-indigo-400 hover:bg-opacity-50"
+          >
+            +event
+          </button>
         </div>
         {show ? <AddEventCard /> : null}
         <ProfileEventCard className="mt-6" />
