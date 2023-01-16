e03143dca48ee8405842eac23258f255c889ace3
Ilwad Kamal <112329600+ilwadk@users.noreply.github.com>
2023-01-16 13:52:48 +0000

    fetched test data and rendered on explore page

--- pages/explore/index.js
+++ pages/explore/index.js
@@ -5,13 +5,29 @@ import SearchBar from "../../components/SearchBar/SearchBar";
 import Filter from "../../components/Filter/Filter";
 import ExploreTiles from "../../components/ExploreTiles/ExploreTiles";
 
-export default function index() {
+export const getStaticProps = async () => {
+
+  const res = await fetch ("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  return {
+    props: { posts: data}
+  }
+}
+
+
+export default function index({ posts}) {
   return (
     <div>
       <SearchBar />
       <Filter />
-      <ExploreTiles />
+      {/* <ExploreTiles /> */}
       <h1>Explore Page</h1>
+      {posts.map (post => (
+        <div key= {post.id}>
+<h3>{post.title}</h3>
+        </div>
+      ))}
       <ExploreList />
     </div>
   );
