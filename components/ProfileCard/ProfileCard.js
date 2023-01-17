60d68939b78e6e6b5a4b2b61f6406ed13a1e9cc7
itsevgenii <smazanovich@icloud.com>
2023-01-17 11:58:16 +0000

    added modal component  for updating user profile logic, created a modal branch

--- components/ProfileCard/ProfileCard.js
+++ components/ProfileCard/ProfileCard.js
@@ -4,6 +4,7 @@ import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function ProfileCard() {
   const { user, error, isLoading } = useUser();
+  const [showModal, setShowModal] = React.useState(false);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
@@ -67,6 +68,84 @@ export default function ProfileCard() {
               <a href={"#"}> CONTACT </a>
             )}
           </button>
+          <div className="userUpdate">
+          <button
+        className="border-indigo-900 text-white bg-indigo-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        type="button"
+        onClick={() => setShowModal(true)}
+      >
+        Update User Page
+      </button>
+      {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+        {/* <!-- Modal content --> */}
+        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+     
+            <div className="px-6 py-6 lg:px-8">
+                <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">Update profile</h3>
+                <form className="space-y-6" action="#">
+                    <div>
+                        <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
+                        <input type="name" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="John Lewis" required/>
+                    </div>
+                    <div>
+                        <label htmlFor="location" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Location</label>
+                        <input type="location" name="location" id="location" placeholder="Higham on the Hill" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required/>
+                    </div>
+                    <div>
+                        <label htmlFor="Email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                        <input type="email" name="Email" id="Email" placeholder="johnlewis@gmail.com" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required/>
+                    </div>
+                    <div>
+                        <label htmlFor="sharer" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sharer?</label>
+                        <input type="checkbox" name="sharer" id="sharer" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                        <label htmlFor="learner" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Learner?</label>
+                        <input type="checkbox" name="learner" id="learner" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required/>
+                    </div>
+                    <div>
+                        <label htmlFor="aboutMe" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Profile Description</label>
+                        <textarea type="text" name="aboutMe" id="aboutMe" placeholder="I am a biology tutor blablabla" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-4 h-48 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required/>
+                    </div>
+                    <div>
+                        <label htmlFor="image" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Upload your photo (not working just now, bear with us)</label>
+                        <input type="file" name="image" id="image" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required/>
+                    </div>
+                </form>
+            </div>
+
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 ">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Save Changes
+                  </button>
+                </div>
+
+        </div>
+
+              </div>
+            </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+
+
+          </div>
         </div>
       </div>
     </div>
