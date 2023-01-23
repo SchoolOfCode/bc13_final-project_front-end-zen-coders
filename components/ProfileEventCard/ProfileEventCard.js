1e79ce61d99bc5863f82be88d778ab6de911c0fd
Ilwad Kamal <112329600+ilwadk@users.noreply.github.com>
2023-01-23 16:06:30 +0000

    small errors with filtering solved

--- components/ProfileEventCard/ProfileEventCard.js
+++ components/ProfileEventCard/ProfileEventCard.js
@@ -39,7 +39,7 @@ export default function ProfileEventCard({title, area, location, description,sta
         </div>
         <div className="col-span-3 rounded-r-lg border-y-2 border-r-2 border-black p-3">
           <div className="flex flex-row">
-            <div className="mr-3 h-14 w-14 rounded-full bg-red-100"></div>
+            {/* <div className="mr-3 h-14 w-14 rounded-full bg-red-100"></div> */}
             <div>
               <h1 className="text-4xl font-bold">{title}</h1>
               <h3 className="text-base font-light">{area},{location}</h3>
