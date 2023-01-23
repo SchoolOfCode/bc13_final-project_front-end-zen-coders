ad6a72f5d6c0abffeb2675316789963d036fb2d5
kianeblackman <kianeblackman@gmail.com>
2023-01-23 15:41:11 +0000

    mobile explore cards icons adjusted

--- components/ExploreCard/ExploreCard.js
+++ components/ExploreCard/ExploreCard.js
@@ -1,11 +1,10 @@
+//* Description: This component is used to display the cards on the explore page. It takes in the title, name, start time and skill as props. It then displays the relevant icon based on the skill
+
+// Import statements
 import React from 'react';
 import Link from 'next/link';
 
-//import all the icons
-//store them in an array
-//assign correct icon to skill
-//if skill = '', img =
-
+// Component
 export default function ExploreCard({ title, name, startTime, skill }) {
   //lists of weekdays and month format .
   const weekdayList = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
@@ -35,35 +34,33 @@ export default function ExploreCard({ title, name, startTime, skill }) {
 
   return (
     <Link href="/profile">
-      <div className="flex flex-col items-center justify-center rounded-3xl border-2 border-black bg-white hover:bg-gray-100 hover:shadow-xl m-3 md:m-6">
-        <div className="m-5 flex border-2 border-black items-center self-end rounded-full">
+      <div className="m-3 flex flex-row items-center justify-center rounded-3xl border-2 border-black bg-white hover:bg-gray-100 hover:shadow-xl md:m-6 md:flex-col">
+        <div className="m-2 flex w-1/4 items-start self-start rounded-full border-2 border-black md:static md:m-5 md:h-20 md:w-20 md:self-end">
           {skill === 'Knitting' ? (
             <img
-              className="h-20 w-20 object-contain p-3"
+              className="object-contain p-1 md:p-3"
               src="/icons/knitting-black.svg"
+              x
             />
           ) : null}
           {skill === 'Music' ? (
-            <img
-              className="h-20 w-20 object-contain p-3"
-              src="/icons/music-black.svg"
-            />
+            <img className="object-contain p-3" src="/icons/music-black.svg" />
           ) : null}
           {skill === 'Gardening' ? (
             <img
-              className="h-20 w-20 object-contain p-3"
+              className="object-contain p-3"
               src="/icons/gardening-black.svg"
             />
           ) : null}
           {skill === 'Photography' ? (
             <img
-              className="h-20 w-20 object-contain p-3"
+              className="object-contain p-3"
               src="/icons/photography-black.svg"
             />
           ) : null}
           {skill === 'Painting' ? (
             <img
-              className="h-20 w-20 object-contain p-3"
+              className="object-contain p-3"
               src="/icons/painting-black.svg"
             />
           ) : null}
@@ -75,39 +72,35 @@ export default function ExploreCard({ title, name, startTime, skill }) {
           ) : null}
           {skill === 'Language' ? (
             <img
-              className="h-20 w-20 object-contain p-3"
+              className="object-contain p-3"
               src="/icons/language-black.svg"
             />
           ) : null}
           {skill === 'Tutoring' ? (
             <img
-              className="h-20 w-20 object-contain p-3"
+              className="object-contain p-3"
               src="/icons/tutoring-black.svg"
             />
           ) : null}
           {skill === 'Sports' ? (
-            <img
-              className="h-20 w-20 object-contain p-3"
-              src="/icons/sport-black.svg"
-            />
+            <img className="object-contain p-3" src="/icons/sport-black.svg" />
           ) : null}
           {skill === 'Other' ? (
-            <img
-              className="h-20 w-20 object-contain p-3"
-              src="/icons/other-black.svg"
-            />
+            <img className="object-contain p-3" src="/icons/other-black.svg" />
           ) : null}
         </div>
-        <div className="m-3 w-full pb-3 px-6">
-          <h1 className="font-bold text-lg w-full">{title}</h1>
-          <p className="font-light text-lg">{name}</p>
+        <div className="w-full p-3">
+          <div>
+            <h1 className="w-full text-lg font-bold">{title}</h1>
+            <p className="text-lg font-light">{name}</p>
+          </div>
           <div className="mt-2.5 flex flex-row">
-            <img className="h-30 w-24 mr-3" src="/icons/arrow-black.svg" />
-            <div className="flex flex-col justify-end">
-              <p>
+            <img className="h-30 mr-3 w-24" src="/icons/arrow-black.svg" />
+            <div className="flex flex-row items-end justify-end gap-3 md:flex-col md:items-start md:gap-0">
+              <p className="flex flex-row">
                 {hour}:{minutes}
               </p>
-              <p>
+              <p className="font-bold">
                 {weekday} {day} {month}
               </p>
             </div>
--- pages/explore/index.js
+++ pages/explore/index.js
@@ -8,7 +8,6 @@ import FilterSm from '../../components/Filter/FilterSm.js';
 export const getServerSideProps = async () => {
   const res = await fetch(process.env.DATABASE_URL);
   const data = await res.json();
-
   return {
     props: { events: data },
   };
@@ -17,11 +16,10 @@ export const getServerSideProps = async () => {
 export default function index({ events }) {
   return (
     <div className="mx-3 mb-20 md:mx-12 md:pt-28">
-      {/* <SearchBar /> */}
       <Filter />
       <FilterSm />
       <div className="flex pt-16 flex-row items-center">
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5">
           {events.map((event) => (
             <div key={event.id}>
               <ExploreCard
