c457cc10823ecfc6885655335777e83d8f272766
kianeblackman <kianeblackman@gmail.com>
2023-01-23 10:18:31 +0000

    text added to filter bar

--- components/Filter/Filter.js
+++ components/Filter/Filter.js
@@ -11,81 +11,99 @@ export default function Filter() {
           </li>
         </Link>
         <Link href="/explore/music">
-          <li className="hover:scale-110 flex items-center justify-center">
-            <div>
-              <img src="/musicIcon.svg" alt="musicIcon" className="w-12" />
-            </div>
+          <li className="hover:scale-110 flex flex-col items-center justify-center">
+            <img
+              src="/musicIcon.svg"
+              alt="musicIcon"
+              className="w-12 bg-black"
+            />
+            <text>Music</text>
           </li>
         </Link>
         <Link href="/explore/knitting">
-          <li className="hover:scale-110 flex items-center justify-center bg-black">
-            <div>
-              <img src="/knitting.svg" alt="knittingIcon" className="w-12" />
-            </div>
+          <li className="hover:scale-110 flex flex-col items-center justify-center ">
+            <img
+              src="/knitting.svg"
+              alt="knittingIcon"
+              className="w-12 bg-black"
+            />
+            <text>Knitting</text>
           </li>
         </Link>
         <Link href="/explore/language">
-          <li className="hover:scale-110 flex items-center justify-center bg-black">
-            <div>
-              <img
-                src="/languageIcon.svg"
-                alt="languageIcon"
-                className="w-12"
-              />
-            </div>
+          <li className="hover:scale-110 flex flex-col items-center justify-center ">
+            <img
+              src="/languageIcon.svg"
+              alt="languageIcon"
+              className="w-12 bg-black bg-black"
+            />
+            <text>Language</text>
           </li>
         </Link>
         <Link href="/explore/sport">
-          <li className="hover:scale-110 flex items-center justify-center bg-black">
-            <div>
-              <img src="/sportIcon.svg" alt="sportIcon" className="w-12" />
-            </div>
+          <li className="hover:scale-110 flex flex-col items-center justify-center ">
+            <img
+              src="/sportIcon.svg"
+              alt="sportIcon"
+              className="w-12 bg-black"
+            />
+            <text>Sport</text>
           </li>
         </Link>
         <Link href="/explore/gardening">
-          <li className="hover:scale-110 flex items-center justify-center bg-black">
-            <div>
-              <img src="/shears.svg" alt="shearsIcon" className="w-12" />
-            </div>
+          <li className="hover:scale-110 flex flex-col items-center justify-center ">
+            <img src="/shears.svg" alt="shearsIcon" className="w-12 bg-black" />
+            <text>Gardening</text>
           </li>
         </Link>
         <Link href="/explore/writing">
-          <li className="hover:scale-110 flex items-center justify-center bg-black">
-            <div>
-              <img src="/writingIcon.svg" alt="writingIcon" className="w-12" />
-            </div>
+          <li className="hover:scale-110 flex flex-col items-center justify-center ">
+            <img
+              src="/writingIcon.svg"
+              alt="writingIcon"
+              className="w-12 bg-black"
+            />
+            <text>Writing</text>
           </li>
         </Link>
         <Link href="/explore/painting">
-          <li className="hover:scale-110 flex items-center justify-center bg-black">
-            <div>
-              <img
-                src="/paintingIcon.svg"
-                alt="paintingIcon"
-                className="w-12"
-              />
-            </div>
+          <li className="hover:scale-110 flex flex-col items-center justify-center ">
+            <img
+              src="/paintingIcon.svg"
+              alt="paintingIcon"
+              className="w-12 bg-black"
+            />
+            <text>Painting</text>
           </li>
         </Link>
         <Link href="/explore/skill/tutor">
-          <li className="hover:scale-110 flex items-center justify-center bg-black">
-            <div>
-              <img src="/tutorIcon.svg" alt="tutorIcon" className="w-12" />
-            </div>
+          <li className="hover:scale-110 flex flex-col items-center justify-center ">
+            <img
+              src="/tutorIcon.svg"
+              alt="tutorIcon"
+              className="w-12 bg-black"
+            />
+            <text>Tutoring</text>
           </li>
         </Link>
         <Link href="/explore/photography">
-          <li className="hover:scale-110 flex items-center justify-center bg-black">
-            <div>
-              <img src="/photoIcon.svg" alt="photoIcon" className="w-12" />
-            </div>
+          <li className="hover:scale-110 flex flex-col items-center justify-center ">
+            <img
+              src="/photoIcon.svg"
+              alt="photoIcon"
+              className="w-12 bg-black"
+            />
+            <text>Photography</text>
           </li>
         </Link>
         <Link href="/explore/other">
-          <li className="hover:scale-110 flex items-center justify-center bg-black">
-            <div>
-              <img src="/otherIcon.svg" alt="otherIcon" className="w-12" />
-            </div>
+          <li className="hover:scale-110 flex flex-col items-center justify-center">
+            <img
+              src="/otherIcon.svg"
+              alt="otherIcon"
+              className="w-12 bg-black"
+            />
+            <text>Other</text>
           </li>
         </Link>
       </div>
