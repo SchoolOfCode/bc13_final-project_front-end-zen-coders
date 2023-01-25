e1df434cee24263fd813d909650f70aacde3249f
Bella Bekanova <108465697+BellaBekanova@users.noreply.github.com>
2023-01-25 10:51:15 +0000

    homepage image and text aligned for a medium screen

--- pages/index.js
+++ pages/index.js
@@ -14,20 +14,20 @@ import Navbar from '../components/Navbar/Navbar';
 export default function Home() {
   return (
     <main>
-      <div className="z-0 flex-col px-6 md:mx-12 md:pt-28">
+      <div className="z-10 md:absolute flex-col px-6 md:mt-12 md:ml-28 md:pt-28">
         <h1 className="mt-3 text-8xl font-normal md:hidden">hobï</h1>
-        <h2 className="mt-3 text-5xl font-bold md:mt-0 md:text-7xl">
+        <h2 className="mt-3 text-5xl font-bold md:text-7xl">
           Learn (almost) anything!
         </h2>
-        <p className="mt-3 font-serif text-lg md:mt-3 md:w-2/3 md:text-xl">
+        <p className="mt-3 font-serif text-lg md:my-6 md:w-2/3 md:text-xl">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat.
         </p>
-        <div className="mt-3 flex items-center gap-6 text-2xl md:mt-3">
+        <div className="mt-3 flex items-center gap-6 text-2xl md:my-12">
           <Link
-            className="rounded-full border-2 border-blue-700 py-1 px-4 text-xl transition ease-in-out hover:bg-blue-400 hover:bg-opacity-50 md:text-2xl"
+            className="rounded-full border-2 border-blue-700 py-2 px-6 text-xl transition ease-in-out hover:bg-blue-400 hover:bg-opacity-50 md:text-2xl"
             href="/explore"
           >
             EXPLORE
@@ -39,10 +39,11 @@ export default function Home() {
             ABOUT
           </Link>
         </div>
-        <div className="">
+       
+      </div>
+      <div className="z-0 md:w-3/4 md:absolute md:top-28 md:-right-8 ">
           <img src="/team-work.svg" alt="home" />
         </div>
-      </div>
     </main>
   );
 }
