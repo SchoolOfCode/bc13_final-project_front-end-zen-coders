d93789336c705fa7e5e1eb8cf7a4379652e9448c
itsevgenii <smazanovich@icloud.com>
2023-01-25 13:58:40 +0000

    testing in progress

--- components/ProfileCard/ProfileCard.test.js
+++ components/ProfileCard/ProfileCard.test.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { render, screen, cleanup, act } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import useUser from "@auth0/nextjs-auth0/client";
+
 import ProfileCard from "./ProfileCard";
-import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { UserProvider, useUser } from "@auth0/nextjs-auth0/client";
+
 
 const user = {
   email: "test@test.com"
@@ -28,25 +29,13 @@ describe("ProfileCard", () => {
         expect(screen.getByText("Loading...")).toBeInTheDocument();
       });
 
-//   test("renders an error message when there is an error", () => {
-//       const error = new Error("Test error");
-//       act(() => {
-//       const { getByText } = render(
-//       <UserProvider value={{ error }}>
-//         
-      <UserProvider>
-        <ProfileCard />
-      </UserProvider>
-    
-//       </UserProvider>
-//     );
-//     })
-//     expect(getByText("Test error")).toBeInTheDocument();
-//   });
 
 test("renders the profile information correctly", async () => {
     const user = {
+        name: "Test User",
         email: "test@example.com",
+        location: "Test Location",
+        aboutMe: "Test about me",
       };
     const event = {
         name: "Test User",
@@ -60,13 +49,13 @@ test("renders the profile information correctly", async () => {
     const authId = "12345";
 
     await act(async () => {
-        const { getByText } = render(
+        const { getByText } =  render(
         <UserProvider value={{ user }}>
           <ProfileCard event={event} userId={userId} authId={authId} />
         </UserProvider>
       );
   
-    //   expect(getByAltText("profile picture")).toBeInTheDocument();
+
       expect(getByText("Test User")).toBeInTheDocument();
       expect(getByText("Test Location")).toBeInTheDocument();
       expect(getByText("Test about me")).toBeInTheDocument();
